e0327961b8859781ecee3ad45c8d1e5d5a87dd04
Isaac Tee <iztyj.work@gmail.com>
2023-11-16 16:51:49 +0800

    Refined ClientSession.java

--- src/task02/src/java/ClientSession.java
+++ src/task02/src/java/ClientSession.java
@@ -102,6 +102,7 @@ public class ClientSession {
                 stop = true;
             }
         }
+        socket.close();
     }
    
     public void writeToServer(Purchase purchase, BufferedWriter bw) throws Exception{
